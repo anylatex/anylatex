0d66a1a628d57fff2d1b9e8b88d570544ed6e167
gyh1621 <cngyh96@gmail.com>
2019-03-06 00:16:28 +0800

    simple login/register interfaces

---
+++ app.js
@@ -0,0 +1,43 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let window = null
+
+// Wait until the app is ready
+app.once('ready', () => {
+  // Create a new window
+  window = new BrowserWindow({
+    // Set the initial width to 800px
+    width: 800,
+    // Set the initial height to 600px
+    height: 600,
+    // Set the default background color of the window to match the CSS
+    // background color of the page, this prevents any white flickering
+    backgroundColor: "#D6D8DC",
+    // Don't show the window until it's ready, this prevents any white flickering
+    show: false
+  })
+
+  // Load a URL in the window to the local index.html path
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, 'app/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Show window when page is ready
+  window.once('ready-to-show', () => {
+    window.show()
+  })
+})
+
+
+global.api_base = "http://latex.0x7cc.com:8080"
+
+const { ipcMain } = require("electron")
+
+ipcMain.on("alert", (event, arg) => {
+    console.log(arg)
+    event.returnValue = 'pong'
+})
