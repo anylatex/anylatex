893c6dd8bda7bc2f740464161dd650ea8d4ed309
gyh1621 <cngyh96@gmail.com>
2019-03-26 23:31:33 +0800

    support insert references; only test for template: `swjtuthesis`

--- app/js/converter.js
+++ app/js/converter.js
@@ -18,6 +18,10 @@ class Converter {
         return this.translatedLatex
     }
 
+    _getDOM(html) {
+        return new DOMParser().parseFromString(html, 'text/html')
+    }
+
     _getParsedDomBodyElements(parsedDom) {
         return parsedDom.children[0].children[1].children
     }
@@ -39,7 +43,7 @@ class Converter {
     }
 
     _convert_elements(html) {
-        var htmlDom = new DOMParser().parseFromString(html, 'text/html')
+        var htmlDom = this._getDOM(html)
         var bodyDom = this._getParsedDomBodyElements(htmlDom)
         if (bodyDom.length == 0) {
             // html is pure text
@@ -82,6 +86,15 @@ class Converter {
                     parsedInnerLatex = this._convert_headings(5, elementContent)
                     latex = latex.replace(outerHTML, parsedInnerLatex)
                     break
+                // Converting references
+                case 'REFERENCE':
+                    if (element.children[0] && element.children[0].tagName == 'SUP') {
+                        parsedInnerLatex = `\\supercite{${element.getAttribute('labels')}}`
+                    } else {
+                        parsedInnerLatex = `\\cite{${element.getAttribute('labels')}}`
+                    }
+                    latex = latex.replace(outerHTML, parsedInnerLatex)
+                    break
                 // Converting others
                 case 'DIV':
                     parsedInnerLatex = `\n${this._convert_elements(element.innerHTML)}\n`
@@ -118,10 +131,6 @@ class Converter {
                     break
                 case 'LI':
                     parsedInnerLatex = `\\item ${this._convert_elements(element.innerHTML)}\n`
-                    latex = latex.replace(outerHTML, parsedInnerLatex)
-                    break
-                case 'SPAN':
-                    parsedInnerLatex = this._convert_elements(element.innerHTML)
                     latex = latex.replace(outerHTML, parsedInnerLatex)
                     break
             }
